7b72350264eb1e88e4a507f820326e57a5e0022a
koss054 <98498436+koss054@users.noreply.github.com>
2022-05-02 23:06:57 +0300

    Upload: Solutions - Eight Kyu

---
+++ EigthKyu/Isograms/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsogram("Dermatoglyphics"));
+            Console.WriteLine(IsIsogram("aba"));
+            Console.WriteLine(IsIsogram("moOse"));
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            List<char> chars = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!Char.IsLetter(str[i])) {
+                    return false;
+                }
+
+                if (chars.Contains(Char.ToLower(str[i]))) {
+                    return false;
+                }
+
+                chars.Add(Char.ToLower(str[i]));
+            }
+
+            return true;
+        }
+    }
+}
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+// Implement a function that determines whether a string that contains only letters is an isogram. 
+// Assume the empty string is an isogram. Ignore letter case.
