09deff15f33ac61b653e080b495adbeb10f97427
koss054 <98498436+koss054@users.noreply.github.com>
2022-05-04 20:32:23 +0300

    Upload: Challenge Solutions

---
+++ K02.SeventhKyu/CamelCaseMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CamelCaseMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CamelCase("hello world"));
+            Console.WriteLine(CamelCase("camel case word"));
+        }
+
+        public static string CamelCase(string str)
+        {
+            string[] words = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string result = string.Empty;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                char firstLetter = Char.ToUpper(currentWord[0]);
+                result += firstLetter.ToString() + currentWord.Substring(1);
+            }
+
+            return result;
+        }
+    }
+
+
+}
---
+++ K02.SeventhKyu/MexicanWave/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MexicanWave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", wave("hello")));
+            Console.WriteLine(string.Join(", ", wave("Two words")));
+            Console.WriteLine(string.Join(", ", wave(" gap ")));
+        }
+
+        public static List<string> wave(string str)
+        {
+            List<string> result = new List<string>();
+            str = str.ToLower();
+
+            for (int i = 0; i < str.Length; i++) {
+                if (String.IsNullOrWhiteSpace(str[i].ToString())) {
+                    continue;
+                }
+
+                char[] chars = str.ToCharArray();
+                chars[i] = Char.ToUpper(str[i]);
+
+                string wordToAdd = new string(chars);
+                result.Add(wordToAdd.ToString());
+            }
+
+            return result;
+        }
+    }
+}
